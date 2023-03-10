3d3f36ac8c47c6d525ec8d62a7312a4313e65d93
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-10 17:27:43 -0600

    Practica 18

---
+++ 18_Tuplas.py
@@ -0,0 +1,7 @@
+color = ['verde', 'rojo', 'amarillo', 'morado1', 'naranja', 'negro', 'blanco', ' verde', 'rosa']
+
+print(color[1])
+
+number = (5,3,11,7)
+
+print('La suma es: ',number[0]+number[1]-number[2]+number[3])
