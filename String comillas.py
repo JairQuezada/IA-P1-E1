6b89d7baebf508c78d413277b6a222de5ec5da67
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-09 22:46:39 -0600

    Revert "Proyecto Primer Parcial IA"
    This reverts commit 51325fbd6a31c27a8ede2e3fbb443e3e4298d505.

--- String comillas.py
+++
@@ -1,7 +0,0 @@
-variable1 = "String con comillas dobles"
-variable2 = 'String con comillas simples'
-
-print(variable1); print(variable2)
-
-frase = '"print()" se utiliza para imprimir valores en la consola'
-print(frase)
--- Variables.py
+++
@@ -1,7 +0,0 @@
-saludo = "Hola mundo"
-print(saludo)
-
-x=3
-y=8
-z=x+y
-print(z)
