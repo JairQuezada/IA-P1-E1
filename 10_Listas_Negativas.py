dfaecb7d682efb79f672fd68b3acbe03dcce6601
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-10 02:13:45 -0600

    Practica 10

---
+++ 10_Listas_Negativas.py
@@ -0,0 +1,7 @@
+color = ['verde', 'rojo', 'amarillo', 'morado1', 'naranja', 'negro', 'blanco', ' verde', 'rosa']
+
+print(color[-1])
+print(color[2])
+print(color[-3])
+print(color[-2])
+print(color[0])
