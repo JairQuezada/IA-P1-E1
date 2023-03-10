057049cbb17e7cabe0a178eb83f3011a85180c68
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-10 02:20:51 -0600

    Practica 13

---
+++ 13_POP.py
@@ -0,0 +1,7 @@
+color = ['verde', 'rojo', 'amarillo', 'morado1', 'naranja', 'negro', 'blanco', 'verde', 'rosa']
+valor1 = color.pop(2)
+valor2 = color.pop(-4)
+print(color)
+
+lista2 = [valor1,valor2]
+print(lista2)
