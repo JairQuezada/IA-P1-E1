7ec9a5cc27a64b51f62a6e528ce263fadaf138e7
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-09 22:45:42 -0600

    Proyecto Primer Parcial IA

---
+++ String comillas.py
@@ -0,0 +1,7 @@
+variable1 = "String con comillas dobles"
+variable2 = 'String con comillas simples'
+
+print(variable1); print(variable2)
+
+frase = '"print()" se utiliza para imprimir valores en la consola'
+print(frase)
