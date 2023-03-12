baafefa1d74606001f0d3830f558be3f1e24e004
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-11 23:15:41 -0600

    Practica 32

---
+++ 32_Funciones_Args.py
@@ -0,0 +1,12 @@
+def colores(*args):
+    print('El color ', args[1], ' es mi favorito. ', 'El color ',args[0], ' tampoco esta mal.')
+    
+colores("azul","amarillo")
+
+def suma(*args):
+    total = 0
+    for x in args:
+        total = total + x
+    print("La suma de los numeros es: ",total)
+    
+suma(4,6,7,9,2)
