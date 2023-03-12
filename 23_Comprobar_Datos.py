8e6d3250f171994d02ca3330955610adb8730407
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-11 22:00:23 -0600

    Practica 23

---
+++ 23_Comprobar_Datos.py
@@ -0,0 +1,8 @@
+color = ['verde', 'rojo', 'amarillo', 'morado1', 'naranja', 'negro', 'blanco', ' verde', 'rosa']
+
+colo = input("Introduce un color. \n")
+
+if colo in color:
+    print("El color "+ colo + " esta admitido")
+else:
+    print("Color no admitido")
