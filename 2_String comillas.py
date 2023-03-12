7b09ef55b03322cf7500baf71f5c683fee99d61b
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-12 16:26:40 -0600

    Practica 2

--- 2_String comillas.py
+++ 2_String comillas.py
@@ -1,6 +1,6 @@
 variable1 = "String con comillas dobles"
 variable2 = 'String con comillas simples'
-
+#Se crean dos variables con nombres para luego hacer un print
 print(variable1); print(variable2)
 
 frase = '"print()" se utiliza para imprimir valores en la consola'
