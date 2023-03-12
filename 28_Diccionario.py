9f8bb08c03872e73958eb838248df0859bc252da
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-11 22:48:17 -0600

    Practica 28

---
+++ 28_Diccionario.py
@@ -0,0 +1,15 @@
+celular1 = {
+    'Categoria': 'Celulares',
+    'Modelo': 'Samsung S23 Ultra',
+    'Precio': '31,000'
+}
+
+celular2 = {
+    'Categoria': 'Celulares',
+    'Modelo': 'iPhone 14 Pro Max',
+    'Precio': '31,000'
+}
+
+consulta1 = celular1["Modelo"]
+consulta2 = celular1["Precio"]
+print("El modelo "+ consulta1 + " cuesta " + consulta2 + " $ .")
