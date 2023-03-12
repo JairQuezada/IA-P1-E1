83c37a0a1dbbcafdbde6a625f404c5a89100f5c2
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-11 20:27:59 -0600

    Practica 20

---
+++ 20_CondicionalIf.py
@@ -0,0 +1,15 @@
+num1 = 10
+num2 = 30
+if num1 != num2:
+    print('Se ejecuta el if 1.')
+
+num1 = 400
+num2 = 4
+
+if num1 > num2:
+    print('Se ejecuta el if 2.')
+    
+num1 = 4
+
+if num1 == num2:
+    print('Se ejecuta el if 3.') 
