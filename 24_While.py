c0fa651a2b2d07a5332cd7a84e3f33c13a295a91
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-11 22:10:45 -0600

    Practica 24

---
+++ 24_While.py
@@ -0,0 +1,18 @@
+print("Primer Ciclo\n")
+x=0
+while x <= 18:
+    print(x)
+    x+=5
+print("\n\n")
+
+print("Segundo Ciclo\n")
+x=0
+while x>= -150:
+    print(x)
+    x -=10
+print("\n\n")
+
+x = 10
+while x >=0:
+    print('El valor del bucle es ' + str(x))
+    x -=1
