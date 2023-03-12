1d2a662ddbf8e6c38de0e57482cc49ab63980eb4
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-11 22:56:54 -0600

    Practica 29

---
+++ 29_DiccionariosFor.py
@@ -0,0 +1,14 @@
+celular1 = {
+    'Categoria': 'Celulares',
+    'Modelo': 'Samsung S23 Ultra',
+    'Precio': '31,000'
+}
+
+celular2 = {
+    'Categoria': 'Celulares',
+    'Modelo': 'iPhone 14 Pro Max',
+    'Precio': '31,000'
+}
+
+for x, y in celular1.items():
+    print(x, ": ", y, ".")
