5cb2690da0e0b81d2bf097afc3d46f514b17a183
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-12 23:01:18 -0600

    Practica 4

--- 4_Tille_Upper.py
+++ 4_Tille_Upper.py
@@ -1,7 +1,8 @@
 nombre = "christian jair quezada ibarra"
 print(nombre.title())
-
+#Con la funcion Title se cambia la primer letra de cada palabra en vocal
 frase = "Esta Frase Se Va a Cambiar"
 
 print(frase.upper())
 print(frase.lower())
+#Aqui se cambia la frase en mayusculas y en minisculas
