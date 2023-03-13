d6dd54d0185e7243e25a294b19d3a8abd4c4eaf3
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-12 23:02:38 -0600

    Practica 5

--- 5_Salto_de_Linea.py
+++ 5_Salto_de_Linea.py
@@ -1,2 +1,3 @@
 print("No, no te preocupes por mí \nAquí todo sigue igual, como cuando estabas tú \nSi, es cierto que no hay calor \nNi en la casa hay el olor de tu cuerpo, sin igual\n")
 print("\tYa, ya la fuente se secó, el canario ya murió \n\tPero aquí, no hay novedad \n\tNo, no te preocupes por mí \n\tAquí todo sigue igual, como cuando estabas tú")
+#con el \n se salta de limea y con \t se hace una tabulacion automatica
