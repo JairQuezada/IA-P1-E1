e7867d095bf66688399a13467f0b4bc7d7a52991
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-12 23:25:09 -0600

    Practica 25

--- 25_While_Break.py
+++ 25_While_Break.py
@@ -8,3 +8,4 @@ while x < 40:
         print("Se rompio la ejecucion del bucle cuando x valia " + str(x))
         break
     print("El valor del bucle es: " + str(x))
+#El break sirve para parar un bucle, ya sea si es con while o con for
