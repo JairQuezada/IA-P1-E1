624011658a417e7d49557dab4d8334a9798be6ad
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-12 23:22:57 -0600

    Practica 23

--- 23_Comprobar_Datos.py
+++ 23_Comprobar_Datos.py
@@ -1,8 +1,10 @@
 color = ['verde', 'rojo', 'amarillo', 'morado1', 'naranja', 'negro', 'blanco', ' verde', 'rosa']
 
 colo = input("Introduce un color. \n")
+#Con el input se puede recibir una entrada del usuario en la consola
 
 if colo in color:
     print("El color "+ colo + " esta admitido")
 else:
     print("Color no admitido")
+#La entrada del usuario se guarda y si algun color concuerda activa el if
