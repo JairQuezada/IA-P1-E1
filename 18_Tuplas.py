e15f308a04bc73b0ede015c86c6226265d5e4ab9
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-12 23:16:35 -0600

    Practica 18

--- 18_Tuplas.py
+++ 18_Tuplas.py
@@ -5,3 +5,4 @@ print(color[1])
 number = (5,3,11,7)
 
 print('La suma es: ',number[0]+number[1]-number[2]+number[3])
+#Las tuplas sirven de manera similar a las listas, pero se declaran de manera diferente
