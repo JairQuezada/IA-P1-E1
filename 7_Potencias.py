25ed9c2ed718f361ec24aeccde6519fe0a61c7e5
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-12 23:04:49 -0600

    Practica 7

--- 7_Potencias.py
+++ 7_Potencias.py
@@ -3,3 +3,5 @@ print(potencia)
 
 multi = 5*5*5*5*5*5
 print(multi)
+#El operador ** se usa para sacar la potencia de manera facil y sencilla 
+#Aunque se puede hacer de manera manual con la multiplicacion
