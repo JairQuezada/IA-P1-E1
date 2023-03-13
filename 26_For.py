5cdde9931cb107a1f07e6f2ae65f0fd8ef122ec3
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-12 23:26:16 -0600

    Practica 26

--- 26_For.py
+++ 26_For.py
@@ -2,3 +2,4 @@ color = ('verde', 'rojo', 'amarillo', 'morado', 'naranja', 'negro', 'blanco', 'v
 
 for x in color:
     print("El color es: " + x + ".")
+#El for al igual que el while sirve para ejecutar un codico hasta que se cumpla una condicion
