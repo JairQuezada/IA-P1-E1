2a7dc34bcf4c0b8054f15cf61218158018f274a7
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-12 22:59:11 -0600

    Practica 3

--- 3_Concatenar Strings.py
+++ 3_Concatenar Strings.py
@@ -1,11 +1,12 @@
 string1 = "Hola"
 string2 = 'planeta'
 string3 = string1+string2
-
+#Se crea dps strings para sumarlos y agregarlo a uno solo
 print(string3)
-
+#Se imprime el string
 string1 = string1 + string2
 
 print(string1)
 
 print("Hola "+ string2 + " :D")
+#Se imprime una frase junto con el string
