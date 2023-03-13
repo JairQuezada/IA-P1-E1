63753f5700cb7a43fd62d744b6a9c01a2af7f7eb
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-12 23:28:13 -0600

    Practica 27

--- 27_For_Range.py
+++ 27_For_Range.py
@@ -1,2 +1,5 @@
 for x in range(5,500, 50):
     print(x)
+#El primer valor del () dice cuanto vale x
+#El segundo dice hasta donde va a llegar la x
+#El tercero menciona cuanto aumenta x por cada ciclo
