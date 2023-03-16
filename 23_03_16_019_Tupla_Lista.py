f10bb0587fb54f3b4736a95833312898ae4b309b
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 13:50:52 -0600

    Practica 19

--- 23_03_16_019_Tupla_Lista.py
+++ 23_03_16_019_Tupla_Lista.py
@@ -1,5 +1,5 @@
 color = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
 tupla = tuple(color)
-
 print(tupla)
+print(type(tupla))
 #Con tuple se convierte una lista en una tupla
