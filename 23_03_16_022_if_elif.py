3927b4124f01f59f9bb832a4f0ac493b749e812e
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 14:06:42 -0600

    Practica 22

--- 23_03_16_022_if_elif.py
+++ 23_03_16_022_if_elif.py
@@ -1,15 +1,15 @@
 edad = int(input("Cual es su edad?\n"))
-
+#Con el input se puede meter un dato desde la terminal
 if edad <=0:
-    print('nadie puede tener esa edad.')
+    print('Nadie puede tener esa edad.')
 elif edad >= 1 and edad < 18:
     print('Eres menor de edad.')
 elif edad >= 18 and edad <= 45:
-    print('Eres una adulto joven.')
+    print('Eres un adulto.')
 elif edad > 45 and edad <= 100:
     print('Eres un adulto mayor.')
 elif edad > 100 and edad <= 120:
-    print('Wow, es muy viejo.')
+    print('Eres muy mayor.')
 else:
     print('Edad no valida.')
 #El elif sirve para una lista de opciones, si no se cumple ninguna se salta al else
