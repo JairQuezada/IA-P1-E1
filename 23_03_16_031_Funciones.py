212539289f63b11bf82a884db28e8a19bd9e994c
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 15:07:58 -0600

    Practica 31

--- 23_03_16_031_Funciones.py
+++ 23_03_16_031_Funciones.py
@@ -1,7 +1,7 @@
 def suma(num1,num2):
     print("El resultado de la suma es: ",num1 + num2)
     
-suma(15,5)
-suma(10,5)
-suma(200,700)
+suma(20,10)
+suma(30,20)
+suma(27000,30000)
 #Con def se puede llamar un codigo mientras encuentre un valor para interactuar
