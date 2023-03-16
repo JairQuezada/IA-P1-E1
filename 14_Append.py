ee5fb960495464de6ab883d1fe33666375b6f1fd
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 13:10:07 -0600

    Practica 14

--- 14_Append.py
+++
@@ -1,7 +0,0 @@
-color = ['verde', 'rojo', 'amarillo', 'morado1', 'naranja', 'negro', 'blanco', ' verde', 'rosa']
-
-color.append('lila')
-color.append('aqua')
-
-print(color)
-#Con append se puede agregar un objeto a una lista
---
+++ 23_03_16_014_Append.py
@@ -0,0 +1,6 @@
+color = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+color.append('fuxia')
+color.append('celeste')
+
+print(color)
+#Con append se puede agregar un objeto a una lista, este se agrega hasat el ultimo de la lista
