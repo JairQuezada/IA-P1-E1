b60bbadef9ab9767e305bb26b176fbed62473e45
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 12:12:45 -0600

    Practica 4

--- 23_03_16_004_Tille_Upper.py
+++ 23_03_16_004_Tille_Upper.py
@@ -1,7 +1,7 @@
-nombre = "christian jair quezada ibarra"
-print(nombre.title())
+nombre = "christian jair quezada ibarra".title()
+print(nombre)
 #Con la funcion Title se cambia la primer letra de cada palabra en vocal
-frase = "Esta Frase Se Va a Cambiar"
+frase = "Esta Es Una Frase Para Ser Formateada."
 
 print(frase.upper())
 print(frase.lower())
