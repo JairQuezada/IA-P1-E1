e04fc6866974b36341983961b03c7b886cf99b8d
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 13:02:06 -0600

    Practica 11

--- 11_Eliminar_Elementos.py
+++
@@ -1,9 +0,0 @@
-color = ['verde', 'rojo', 'amarillo', 'morado1', 'naranja', 'negro', 'blanco', ' verde', 'rosa']
-
-del color[2]
-del color[5]
-del color[-3]
-del color[-1]
-
-print(color)
-#Con del se puede eliminar un objeto en la lista y se selecciona con []
---
+++ 23_03_16_011_Eliminar_Elementos.py
@@ -0,0 +1,8 @@
+color = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+del color[1]
+del color[4]
+del color[-4]
+del color[-3]
+
+print(color)
+#Con del se puede eliminar un objeto en la lista y se selecciona con []
