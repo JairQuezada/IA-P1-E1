883a0254880dd20954d2c3d07b3830dd80af642d
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 12:54:54 -0600

    Practica 9

--- 9_Listas.py
+++
@@ -1,5 +0,0 @@
-lista = ["cuatro", "tres", "uno",  "cinco", "dos"]
-print(lista)
-print(lista[0])
-#Las listas on bastantes importantes y se puede imprimir toda la lista completa
-#Con [] puedes seleccionar que numero de objeto en la lista se quiere imprimir
