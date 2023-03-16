14c329ebc278b7436e79638a109d773e52a26de8
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 13:08:08 -0600

    Practica 13

--- 13_POP.py
+++
@@ -1,8 +0,0 @@
-color = ['verde', 'rojo', 'amarillo', 'morado1', 'naranja', 'negro', 'blanco', 'verde', 'rosa']
-valor1 = color.pop(2)
-valor2 = color.pop(-4)
-print(color)
-
-lista2 = [valor1,valor2]
-print(lista2)
-#Con pop se puede sacar la informacion de la lista
---
+++ 23_03_16_013_POP.py
@@ -0,0 +1,8 @@
+color = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+valor1 = color.pop(1)
+valor2 = color.pop(-2)
+print(color)
+#Con pop se puede sacar la informacion de la lista y guardarlo en una variable
+lista = [valor1,valor2]
+print(lista)
+#Se crea una nueva lista con los valores que sacamos de la principal
