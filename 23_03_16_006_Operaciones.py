9e61efa3c4125f9e00a3b63bd43b9aaeab5596e0
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 12:43:24 -0600

    Practica 6

---
+++ 23_03_16_006_Operaciones.py
@@ -0,0 +1,15 @@
+#Las operaciones se hacen bastante facil gracias a cada operador
+suma = 23+30+20+14
+print(suma)
+
+resta = 20+23-50-35-45
+print(resta)
+
+multi = (20*23*2)*(87/92)
+print(multi)
+
+division = (5000/230)/(50/23)
+print(division)
+
+operacion = 10/5+15-17
+print(operacion)
