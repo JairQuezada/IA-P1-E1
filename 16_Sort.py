a0ae04e394acc614313b90dabb25b7438dc62df9
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 13:33:06 -0600

    Practica 16

--- 16_Sort.py
+++
@@ -1,6 +0,0 @@
-color = ['verde', 'rojo', 'amarillo', 'morado', 'naranja', 'negro', 'blanco', 'verde', 'rosa']
-
-color.sort(reverse=True)
-
-print(color)
-#Con sort se acomoda poralfabeto y con el reverse empieza z hasta la a
---
+++ 23_03_16_016_Sort.py
@@ -0,0 +1,5 @@
+color = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+color.sort(reverse=True)
+
+print(color)
+#Con sort se acomoda la lista por alfabeto y con el reverse empieza z hasta la a
