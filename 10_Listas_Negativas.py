b1e718b16e4294a8e6dbe67303c36c0e8db4d529
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 12:59:25 -0600

    Practica 10

--- 10_Listas_Negativas.py
+++
@@ -1,8 +0,0 @@
-color = ['verde', 'rojo', 'amarillo', 'morado1', 'naranja', 'negro', 'blanco', ' verde', 'rosa']
-
-print(color[-1])
-print(color[2])
-print(color[-3])
-print(color[-2])
-print(color[0])
-#Con [] puedes sellecionar un objeto en la lista en especifico y con el negativo empieza al revez
---
+++ 23_03_16_010_Listas_Negativas.py
@@ -0,0 +1,7 @@
+color = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(color[-1])
+print(color[-7])
+print(color[-5])
+print(color[-2])
+print(color[-10])
+#Con [] puedes sellecionar un objeto en la lista en especifico y con el negativo empieza al revez
