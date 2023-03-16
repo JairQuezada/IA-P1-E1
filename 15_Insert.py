1d3928b87e4ed8119c2e29cfb3db88359efe47be
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 13:14:05 -0600

    Practica 15

--- 15_Insert.py
+++
@@ -1,7 +0,0 @@
-color = ['verde', 'rojo', 'amarillo', 'morado1', 'naranja', 'negro', 'blanco', ' verde', 'rosa']
-
-color.insert(0,"oliva")
-color.insert(-3,'plateado')
-
-print(color)
-#Con insert se agrega un dato a la lista, pero aqui puedes sellecionar una posicion en especifico
---
+++ 23_03_16_015_Insert.py
@@ -0,0 +1,6 @@
+color = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+color.insert(-4,"magenta")
+color.insert(-1,'turquesa')
+
+print(color)
+#Con insert se agrega un dato a la lista, pero aqui puedes sellecionar una posicion en especifico
