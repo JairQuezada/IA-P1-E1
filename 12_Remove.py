5dfed8b9107437d0c88f9caf734ae983d6a999c5
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 13:04:25 -0600

    Practica 12

--- 12_Remove.py
+++
@@ -1,8 +0,0 @@
-color = ['verde', 'rojo', 'amarillo', 'morado1', 'naranja', 'negro', 'blanco', ' verde', 'rosa']
-
-color.remove('verde')
-color.remove('rosa')
-
-print(color)
-
-#Con remove se elimina un objeto por el nombre y no con posicion, como lo hace []
---
+++ 23_03_16_012_Remove.py
@@ -0,0 +1,7 @@
+color = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+color.remove('amarillo')
+color.remove('rojo')
+
+print(color)
+
+#Con remove se elimina un objeto por el nombre y no con posicion, como lo hace []
