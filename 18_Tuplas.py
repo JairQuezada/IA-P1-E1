3d607bbcebb78b1bf6889468c12683f6d0dacbf9
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 13:43:50 -0600

    Practica 18

--- 18_Tuplas.py
+++
@@ -1,8 +0,0 @@
-color = ['verde', 'rojo', 'amarillo', 'morado1', 'naranja', 'negro', 'blanco', ' verde', 'rosa']
-
-print(color[1])
-
-number = (5,3,11,7)
-
-print('La suma es: ',number[0]+number[1]-number[2]+number[3])
-#Las tuplas sirven de manera similar a las listas, pero se declaran de manera diferente
---
+++ 23_03_16_018_Tuplas.py
@@ -0,0 +1,8 @@
+color = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print(color[1])
+
+number = (10,1,5,11)
+
+print('La suma es: ',number[0]-number[1]+number[2]+number[3])
+#Las tuplas sirven de manera similar a las listas, pero estas se declaran con ()
+#Las tuplas son inmutables
