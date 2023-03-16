67b7fec857481fe8dd2bae2304d907bc503035ef
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-16 12:59:42 -0600

    Practica 9

--- 23_03_16_009_Listas.py
+++ 23_03_16_009_Listas.py
@@ -3,4 +3,4 @@ print(colores[3])
 lista = ["tres", "dos", "cinco",  "cuatro", "uno"]
 print(lista)
 #Las listas on bastantes importantes y se puede imprimir toda la lista completa
-#Con [] 23_03_16_00 puedes seleccionar que numero de objeto en la lista se quiere imprimir
+#Con [] puedes seleccionar que numero de objeto en la lista se quiere imprimir
